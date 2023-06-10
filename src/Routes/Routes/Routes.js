65ca5918f1379df247fc2f7142ec550950a4db0b
iambijoy656 <bijoychandrodas56@gmail.com>
2023-06-10 10:46:40 +0600

    footer done

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,19 +1,33 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
 import Home from "../../pages/Home/Home/Home";
+import Portfolio from "../../pages/Portfolio/Portfolio";
+import Blog from "../../pages/Blog/Blog";
+import Contact from "../../pages/Contact/Contact";
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Main></Main>,
-        children:[
-            {
-                path:"/",
-                element:<Home></Home>
-            }
-        ]
-    }
-])
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/portfolio",
+        element: <Portfolio></Portfolio>,
+      },
+      {
+        path: "/blog",
+        element: <Blog></Blog>,
+      },
+      {
+        path: "/contact",
+        element: <Contact></Contact>,
+      },
+    ],
+  },
+]);
 
-
-export default router
+export default router;
---
+++ src/pages/Portfolio/Portfolio.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Portfolio = () => {
+  return (
+    <div>
+      <h1 className="text-3xl">This is fortfolio page</h1>
+    </div>
+  );
+};
+
+export default Portfolio;
